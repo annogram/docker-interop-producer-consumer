7596c247ae9404a603a8ae97d5aaf8a1cb080b6d
Akram <Akramd@datacom.co.nz>
2019-04-08 11:12:53 +1200

    Add the correct libraries needed to do fifo logic

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+constexpr auto COMMAND_PIPE = "/var/interop-volume/pipe.fifo";
 
 int main() {
-    std::cout << "hello wrold" << std::endl;
+    pid_t fifo = mkfifo(COMMAND_PIPE, 666);
+    close(fifo);
+    unlink(COMMAND_PIPE);
+    return 1;
 }
