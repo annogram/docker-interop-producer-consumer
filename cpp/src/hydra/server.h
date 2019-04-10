a2b5c6955443243c24b013f3348f967b536754d6
Akram <Akramd@datacom.co.nz>
2019-04-11 10:16:33 +1200

    Create open socket exception for hydra

--- cpp/src/hydra/server.h
+++ cpp/src/hydra/server.h
@@ -11,6 +11,17 @@ constexpr auto PIPE_VOLUME = "/var/interop-volume";
 
 namespace hydra
 {
+class OpenSocketException : public std::exception
+{
+private:
+  std::string _msg;
+
+public:
+  OpenSocketException(std::string msg = "Could not create initial socket") : _msg(msg)
+  {
+  }
+  virtual char const *what() const noexcept { return _msg.c_str(); }
+};
 class server
 {
 private:
