1309c66c7db273d5ef223cf464603a4d7d4af649
Akram <Akramd@datacom.co.nz>
2019-04-11 14:56:38 +1200

    Get first inter-process communication going

--- cpp/src/hydra/server.cpp
+++ cpp/src/hydra/server.cpp
@@ -16,20 +16,24 @@ hydra::server::~server() {}
 void hydra::server::watcher()
 {
     pid_t pipe_id;
-    while (true)
-    {
-        pipe_id = open(_commandPipe.c_str(), O_RDONLY | O_NONBLOCK);
-        std::string depth;
-        read(pipe_id, &depth, sizeof(depth));
-        close(pipe_id);
-        std::cout << depth << std::endl;
-    }
-    
+    int num;
+    char value[300];
+    std::cout << "Awaiting a client" << std::endl;
+    pipe_id = open(_commandPipe.c_str(), O_RDONLY);
+    std::cout << "Found a client" << std::endl;
+    do {
+        if((num = read(pipe_id, value, sizeof(value))) == -1){
+            throw hydra::OpenSocketException("Could not read from pipe");
+        }else{
+            std::cout << value << std::endl;
+        }
+    } while(num > 0);
 }
 
 pid_t hydra::server::startup()
 {
-    _commandFifo = mkfifo(_commandPipe.c_str(), 666);
+    std::cout << "Opening command pipe" << std::endl;
+    _commandFifo = mkfifo(_commandPipe.c_str(), 0666);
     commandWatcher = std::thread(&hydra::server::watcher, this);
     return _commandFifo;
 }
--- python/src/hydra/consumer.py
+++ python/src/hydra/consumer.py
@@ -5,4 +5,4 @@ class Client(object):
     def __init__(self, command_pipe_path='/var/interop-volume/command.fifo'):
         with open(command_pipe_path, 'w') as command_pipe:
             while True:
-                command_pipe.writelines('test')
+                command_pipe.writelines('test\n')
